7f993678c9c4b168fc2d3587f6ad44206e798c3f
Mayur T <mayurthube@gmail.com>
2025-08-18 12:25:08 +0530

    Config changes
    Co-Authored-By: mayurthube-mps <226249637+mayurthube-mps@users.noreply.github.com>

--- src/main/java/com/vedvix/notification/config/RabbitConfig.java
+++ src/main/java/com/vedvix/notification/config/RabbitConfig.java
@@ -7,16 +7,19 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
+/*
 
     @Value("${notification.queues.email}")
     private String emailQueue;
-
+*/
     @Value("${notification.queues.sms}")
     private String smsQueue;
-
+/*
     @Value("${notification.queues.push}")
     private String pushQueue;
 
+
+*/
     @Value("${notification.exchange}")
     private String exchangeName;
 
@@ -24,16 +27,17 @@ public class RabbitConfig {
     public DirectExchange notificationExchange() {
         return new DirectExchange(exchangeName);
     }
-
+    @Bean
+    public Queue smsNotificationQueue() {
+        return new Queue(smsQueue);
+    }
+/*
     @Bean
     public Queue emailNotificationQueue() {
         return new Queue(emailQueue);
     }
 
-    @Bean
-    public Queue smsNotificationQueue() {
-        return new Queue(smsQueue);
-    }
+
 
     @Bean
     public Queue pushNotificationQueue() {
@@ -47,17 +51,19 @@ public class RabbitConfig {
                 .with("notify.email");
     }
 
-    @Bean
-    public Binding bindSmsQueue() {
-        return BindingBuilder.bind(smsNotificationQueue())
-                .to(notificationExchange())
-                .with("notify.sms");
-    }
+
 
     @Bean
     public Binding bindPushQueue() {
         return BindingBuilder.bind(pushNotificationQueue())
                 .to(notificationExchange())
                 .with("notify.push");
+    }*/
+
+    @Bean
+    public Binding bindSmsQueue() {
+        return BindingBuilder.bind(smsNotificationQueue())
+                .to(notificationExchange())
+                .with("notify.sms");
     }
 }
