84d425a40c22827262e8cda09b72ba796dc96cf4
Mayur T <mayurthube@gmail.com>
2025-08-20 16:42:20 +0530

    Changes to run SMS notification

--- src/main/java/com/vedvix/notification/config/RabbitConfig.java
+++ src/main/java/com/vedvix/notification/config/RabbitConfig.java
@@ -1,6 +1,9 @@
 package com.vedvix.notification.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -65,5 +68,17 @@ public class RabbitConfig {
         return BindingBuilder.bind(smsNotificationQueue())
                 .to(notificationExchange())
                 .with("notify.sms");
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
     }
 }
